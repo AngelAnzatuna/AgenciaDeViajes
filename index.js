2a9dc8b5028332f42393f34dddc8f21a74dba6fd
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-25 13:36:23 +0100

    feat: habilitar import y export, y agrega rutas básicas con Express

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Inicio');
+})
+app.get('/nostros', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Nosotros');
+})
+app.get('/contacto', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.send('Contacto');
+})
 
 app.listen(port, () => {
     console.log(`El Servidor esta funcionando en el puerto ${port}`)
