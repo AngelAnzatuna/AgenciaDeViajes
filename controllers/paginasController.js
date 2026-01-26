64d5ae367dd9277194d7a74a73ec214c12aadaca
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-27 00:13:11 +0100

    feat: consulta viajes desde la base de datos y los renderiza en la vista

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,3 +1,5 @@
+import { Viaje } from "../models/Viaje.js";
+
 const paginaInicio = (req, res) => {
     res.render('inicio', {
         pagina: 'Inicio',
@@ -11,10 +13,16 @@ const paginaNosotros = (req, res) => {
     });
 }
 
-const paginaViajes = (req, res) => {
+const paginaViajes = async (req, res) => {
+
+    // Consultar BD
+    const viajes = await Viaje.findAll();
+
+    console.log(viajes);
 
     res.render('viajes', {
-        pagina: 'Viajes'
+        pagina: 'Proximos Viajes',
+        viajes
     });
 }
 
